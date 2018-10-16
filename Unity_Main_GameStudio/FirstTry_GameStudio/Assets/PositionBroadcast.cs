ff46ebb79f39d903ff0967ae606c51bbffe40bb0
harrisonCassar - Desktop <harrison.cassar@gmail.com>
2018-10-16 00:26:35 -0700

    Updating Gravoty Scripts

---
+++ Unity_Main_GameStudio/FirstTry_GameStudio/Assets/PositionBroadcast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionBroadcast : MonoBehaviour {
+
+    public static float x_position = 0;
+    public static float y_position = 0;
+    public static float z_position = 0;
+    public static Vector3 object_position;
+    
+
+    private Rigidbody pulledobject_rb;
+
+	// Use this for initialization
+	void Start () {
+        pulledobject_rb = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        object_position = transform.position;
+        pulledobject_rb.AddForce(RealGravity.appliedForce * Time.deltaTime);
+    }
+}
---
+++ Unity_Main_GameStudio/FirstTry_GameStudio/Assets/RealGravity.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealGravity : MonoBehaviour
+{
+    //public float ForceMultiplier = 1;
+    //private float force = 1;
+
+    private float x_object;
+    private float y_object;
+    private float z_object;
+    private float distance;
+    public static Vector3 appliedForce;
+    private Vector3 pos;
+    public float baseGravityForce = 1;
+    private float appliedForceScalar = 1;
+
+    public Vector3 positionalDifference;
+
+    //private Rigidbody player_rb;
+
+    //public float;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x_object = PositionBroadcast.x_position;
+        y_object = PositionBroadcast.y_position;
+        z_object = PositionBroadcast.z_position;
+
+        distance = PositionBroadcast.object_position.magnitude - transform.position.magnitude;
+
+        appliedForceScalar = baseGravityForce / (distance * distance);
+
+        positionalDifference = pos - PositionBroadcast.object_position;
+
+        appliedForce = positionalDifference.normalized * appliedForceScalar;
+    }
+}
